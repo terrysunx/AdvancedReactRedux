78ce38bb1d76500a470b7bf72e6b9b4a55e0db13
Fuzhen Wang <terrysunx@gmail.com>
2018-07-15 20:35:22 +1000

    Test text area can be typed in

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -5,7 +5,7 @@ import CommentBox from 'components/Comment-Box';
 let component;
 
 beforeEach(() => {
-    component = mount(<CommentBox/>);
+    component = mount(<CommentBox />);
 });
 
 afterEach(() => {
@@ -20,3 +20,14 @@ it('has a textarea and a button', () => {
     expect(component.find('button').length).toEqual(1);
 });
 
+it('has a text area that users can type in', () => {
+    //when trigger event, we use html event name: "change"
+    component.find('textarea').simulate('change', {
+        target: { value: "test" }
+    });
+    //force the component to update? set state render component async
+    //so we have to force it to make sure it is rerendered.
+    component.update();/*defined by enzyme*/
+    //asertion
+    expect(component.find('textarea').prop('value')).toEqual('test');
+})
